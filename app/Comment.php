0a87cd5ad0d8edaeb53ec3f332f6bc57bbdcc68c
Richard Ting <richard.tin@gmail.com>
2016-07-03 13:37:40 +0000

    Add fillable fields for Model

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'sub_title',
+        'content',
+        'is_feature',
+        'page_view',
+    ];
 }
